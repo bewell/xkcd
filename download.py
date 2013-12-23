72bb4992931bdbb03ce614418b3fd7404ec32501
bewell <v.pankauskas@gmail.com>
2013-12-23 18:41:42 +0200

    Create download.py

---
+++ download.py
@@ -0,0 +1,28 @@
+import csv
+from BeautifulSoup import BeautifulSoup
+import urllib
+import re
+
+writer = csv.writer(open("d:/python/notes.csv", 'wb'))
+writer.writerow(["Name","AltText","path"])
+
+
+def getStuff(number, csv):
+	xkcdSite = urllib.urlopen('http://www.xkcd.com/%s' % number)
+	soup = BeautifulSoup(xkcdSite)
+	imageurlcontent = soup.find('div', {"id": "comic"}).find("img")
+#Find the http link and save the jpg
+	url = re.search("http.+\"\ t", str(imageurlcontent))
+	imgUrl = url.group(0).replace("\" t", "")
+	urllib.urlretrieve(imgUrl, "comics/" + str(number) + ".jpg")
+	print 'Finish Image ' + str(number)
+#Find the title text and save into the csv file
+	altTextSearch = re.search("title.+\ alt=", str(imageurlcontent))
+	altText = altTextSearch.group(0)[:-6].replace('title="', "")
+	csv.writerow([str(number),altText,"/comics/" + str(number) + ".jpg"])
+	print 'Finish Text ' + str(number)
+
+for count in range(1, 1305):
+        if(count == 404):
+           continue
+	getStuff(count, writer)
